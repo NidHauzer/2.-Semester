c10857a145c96752881124d6a6a49fe4b6d33931
jonasvbiegel <10519146@ucn.dk>
2024-11-27 16:20:48 +0100

    Create Address.java

---
+++ WarehouseManagement/src/model/Address.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Address {
+	private String streetName;
+	private int houseNo;
+	private String zip;
+	private String city;
+	private String country;
+	
+	public Address(String streetName, int houseNo, String zip, String city, String country) {
+		this.streetName = streetName;
+		this.houseNo = houseNo;
+		this.zip = zip;
+		this.city = city;
+		this.country = country;
+	}
+	
+	public String getStreetName() {
+		return streetName;
+	}
+	public int getHouseNo() {
+		return houseNo;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public String getCity() {
+		return city;
+	}
+	public String getCountry() {
+		return country;
+	}
+}
