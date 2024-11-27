beac328d0099b5bf5d6da20bde4228aea939957d
jonasvbiegel <10519146@ucn.dk>
2024-11-27 16:16:55 +0100

    Create Product.java

--- WarehouseManagement/src/model/Product.java
+++ WarehouseManagement/src/model/Product.java
@@ -2,4 +2,49 @@ package model;
 
 public class Product {
 	private String barcode;
+	private int quantityInStock;
+	private int minStock;
+	private String type;
+	private String colour;
+	private int length;
+	private int amount;
+	
+	public Product(String barcode, int quantityInStock, int minStock, String type, String colour, int length, int amount) {
+		this.barcode = barcode;
+		this.quantityInStock = quantityInStock;
+		this.minStock = minStock;
+		this.type = type;
+		this.colour = colour;
+		this.length = length;
+		this.amount = amount;
+	}
+	
+	public String getBarcode() {
+		return barcode;
+	}
+	public int getQuantityInStock() {
+		return quantityInStock;
+	}
+	public int getMinStock() {
+		return minStock;
+	}
+	public String getType() {
+		return type;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public int getLength() {
+		return length;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void setQuantityInStock(int q) {
+		this.quantityInStock = q;
+	}
+	public void setMinStock(int m) {
+		this.minStock = m;
+	}
 }
