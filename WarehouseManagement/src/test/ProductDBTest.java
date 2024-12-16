bfc51ec526f895b505cdbe6ca0efa8a049212d3c
Jonas Vittrup Biegel <10519146@ucn.dk>
2024-12-16 11:24:06 +0000

    All tests implemented, functioning as of now

--- WarehouseManagement/src/test/ProductDBTest.java
+++ WarehouseManagement/src/test/ProductDBTest.java
@@ -1,14 +1,31 @@
 package test;
 
+import db.ProductDB;
+import db.ProductDBIF;
+import model.Product;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class ProductDBTest {
+	
+	static ProductDBIF pdb;
 
+	@BeforeAll
+	public static void setUp() {
+		pdb = new ProductDB();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void findProductTest() throws SQLException {
+		String barcode = "AAA123";
+		String type = "Tape Extension";
+		Product p = pdb.findByBarcode(barcode);
+		assertEquals(p.getType(), type);
 	}
 
 }
--- WarehouseManagement/src/test/ReceiverDBTest.java
+++ WarehouseManagement/src/test/ReceiverDBTest.java
@@ -1,14 +1,29 @@
 package test;
 
+import db.ReceiverDB;
+import db.ReceiverDBIF;
+import model.Receiver;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class ReceiverDBTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	static ReceiverDBIF rdb;
+	
+	@BeforeAll
+	public static void setUp() {
+		rdb = new ReceiverDB();
 	}
 
+	@Test
+	void findReceiverTest() throws SQLException {
+		String name = "Roskilde Lager";
+		Receiver r = rdb.findByPhoneNo("12345678");
+		assertEquals(r.getName(), name);
+	}
 }
